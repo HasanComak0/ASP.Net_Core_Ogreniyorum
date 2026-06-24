8992efc8aa6431f92775b067ea38bd42981fa80c
Hasan Çomak <hsncomak2222@gmail.com>
2026-06-24 21:51:37 +0300

    bolum4 devam

--- bolum4-MVC Temelleri/ContactApp/Controllers/ContactsController.cs
+++ bolum4-MVC Temelleri/ContactApp/Controllers/ContactsController.cs
@@ -1,13 +1,14 @@
-﻿using ContactApp.Services;
+﻿using ContactApp.Models;
+using ContactApp.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContactApp.Controllers
 {
     public class ContactsController : Controller
     {
-        
+
         private readonly IContactRepository _repo; //bunun newlenmesi lazım
-        private readonly ILogger<ContactsController> _logger;//bunu da newlememiz lazım
+        private readonly ILogger<ContactsController> _logger;//bunu da newlememiz lazım //bu dalga console'a log basıyo amk
 
         //Constructor Injection yapmış olduk
         public ContactsController(IContactRepository repo, ILogger<ContactsController> logger)//o yüzden bu sınıfın Constructorunda newleme işlemi yaptık
@@ -24,19 +25,104 @@ namespace ContactApp.Controllers
         }
         public IActionResult Details(int id)//endpoint tanımı; Bir kişinin idye bağlı olarak gelecek olan bilgileri...
         {
-            return View();
+            var contact = _repo.GetById(id);
+            if (contact is null)
+                return NotFoundView();
+
+            ViewData["Title"] = "Kişi Güncelle";
+            return View(contact);
         }
+
+        private IActionResult NotFoundView()//Burası private olduğu için urlye direk bunu yazarak ulaşamıyoruz
+        {
+            Response.StatusCode = 404;
+            ViewData["Title"] = "Bulunamadı";
+            ViewBag.Message = "Kişi Bulunamadı";
+            return View("NotFound");
+        }
+
+        [HttpGet("create")]//get işlemi için
         public IActionResult Create()//rehbere kişi eklemek için
         {
-            return View();
+            ViewData["Title"] = "Yeni Kişi";
+            return View(new Contact());
         }
+
+        [HttpPost("create")]//post işlemi için
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Contact contact)//rehbere kişi eklemek için
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["Title"] = "Yeni Kişi";
+                return View(contact);
+            }
+            _repo.Add(contact);
+            _logger.LogInformation($"Kişi Eklendi: {contact.Id} {contact.FirstName} {contact.LastName}");
+            TempData["Success"] = "Kayıt Eklendi";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("edit/{id}")]//süslü parantez içerisindeki kısım ROUTEDATA çünkü bunu url üzerinden alıcaz
         public IActionResult Edit(int id)//var olan kişiyi düzenlemek için
         {
-            return View();
+            var contact = _repo.GetById(id);
+            if (contact is null)
+                return NotFoundView();
+            ViewData["Title"] = "Kişi Görüntüleme";
+            return View(contact);//kişiyi web sayfasına yani view'a gönderdik
         }
+
+        [HttpPost("edit/{id}")]//idye göre çalışcak yani
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                ModelState.AddModelError(string.Empty, "Geçersiz istek!");
+            }
+            if (!ModelState.IsValid) 
+            {
+                ViewData["Title"] = "Kişi Güncelle";
+                return View(contact);
+            }
+
+            var ok = _repo.Update(contact);
+
+            if (!ok)
+                return NotFoundView();
+
+            _logger.LogInformation($"Kişi Güncellendi. Id: {contact.Id} {contact.FirstName} {contact.LastName}");
+            TempData["Success"] = "Kayıt Güncellendi!";//_Layout.cshtml içinde var eğer Success doluysa şu şu mesajı göster anlamında
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet("delete/{id}")]
         public IActionResult Delete(int id)//kişi silme
         {
-            return View();
+            var contact = _repo.GetById(id);
+            if (contact is null)
+                return NotFoundView();
+
+            ViewData["Title"] = "Silme Onayı";
+            return View(contact);
+        }
+
+        //burda ActionName kullandık çünkü üstteki fonskiyonla çakışıcaktı çünkü aynı parametreyeler sahipler diğerlerinde farklı parametrelere sahipler
+        [HttpPost("delete/{id}")]
+        [ActionName("Delete")]//üsttekiyle çakışmasın diye bunu yazdık ama nasıl çalışıyo idk
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirm(int id)//kişi silme onay
+        {
+            var ok = _repo.Delete(id);
+            if(!ok)
+                return NotFoundView();
+
+            TempData["Success"] = "Kayıt Silindi";//bununla galiba _Layout.cshtml'e mesaj gönderiyoz o da tarayıcıda mesaj atıyo
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
